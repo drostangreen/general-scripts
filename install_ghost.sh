0d424bb3561ff38587afcbf3bf3656cd07f7bcfc
drostangreen <t.w.green29@gmail.com>
2024-11-13 10:33:36 -0600

    added self signed certs

--- install_ghost.sh
+++ install_ghost.sh
@@ -36,8 +36,15 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS';
 FLUSH PRIVILEGES;
 EOF
 
+# Create Self Signed Certs
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+
 # Install Ghost-CLI with npm
 npm install ghost-cli@latest -g
 
 echo "Install complete. Login as user then move to $INSTALL_DIR and run following: ghost install"
 echo "You will complete the install from cmdline there"
+echo "Add the following lines to the nginx config to use self signed certs if requested:"
+echo "ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;"
+echo "ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;"
+echo "Port will need to be changed from 80 to 443 ssl as well."

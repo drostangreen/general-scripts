41b73899bbdaa1c5e09d787526913a95ce5281ca
drostangreen <t.w.green29@gmail.com>
2024-12-28 19:06:35 -0600

    reworked script to only convert files as needed

--- mp4tomov.sh
+++ mp4tomov.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-directory=$1
+file=$1
+filename="${file%.*}"
 
-mkdir $directory/Proxy
-mv $directory/*.LRF $directory/Proxy/
-cd $directory/Proxy
-
-for file in *.LRF; do
-	filename="${file%.*}"
-	mv "$file" "$filename.MP4"
-done
-
-for file in *.MP4; do
-	filename="${file%.*}"
-	ffmpeg -i $file -c:v copy -c:a pcm_s16le $filename.mov
-done
-
-cd $directory
-for file in *.MP4; do
-	filename="${file%.*}"
-	ffmpeg -i $file -c:v copy -c:a pcm_s16le $filename.mov
-done
+ffmpeg -i $file -c:v copy -c:a pcm_s16le $filename.mov
 
 echo done
